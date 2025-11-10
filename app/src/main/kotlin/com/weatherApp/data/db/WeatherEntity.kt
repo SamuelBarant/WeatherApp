211d5066fddf1c6d3c23bba96c241eab3ec07c9e
SamuelBarant <samucraft346@gmail.com>
2025-11-10 19:07:37 +0100

    Modify the WeatherEntity

--- app/src/main/kotlin/com/weatherApp/data/db/WeatherEntity.kt
+++ app/src/main/kotlin/com/weatherApp/data/db/WeatherEntity.kt
@@ -1,10 +1,14 @@
 package com.weatherApp.data.db
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 //TODO define WeatherEntity as a Room entity
 
+@Entity(tableName = "weather_data")
 data class WeatherEntity(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val time: String,
     val temperature: Double
 )
