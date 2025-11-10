41a928faa65c289536ee867281778b002d881775
SamuelBarant <samucraft346@gmail.com>
2025-11-10 19:18:48 +0100

    Implements Room in the WeatherDao.kt

--- app/src/main/kotlin/com/weatherApp/data/db/WeatherDao.kt
+++ app/src/main/kotlin/com/weatherApp/data/db/WeatherDao.kt
@@ -1,6 +1,10 @@
 package com.weatherApp.data.db
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 //TODO define WeatherDao interface
 /**
@@ -11,8 +15,14 @@ import androidx.lifecycle.LiveData
  * clearDatabase: deletes all the data on the weather_data table
  */
 
+@Dao
 interface WeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertWeatherData(data: List<WeatherEntity>)
+
+    @Query("SELECT * FROM weather_data ORDER BY time ASC")
     fun retrieveWeatherData(): LiveData<List<WeatherEntity>>
+
+    @Query("DELETE FROM weather_data")
     suspend fun clearDatabase()
 }
